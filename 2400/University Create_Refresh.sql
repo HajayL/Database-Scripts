27166d8e98e976e2ec39bee7ca16cb6cf0d4a232
HajayL <84947010+HajayL@users.noreply.github.com>
2024-09-25 12:56:24 -0600

    Changed very few things in University, added composite notes

--- 2400/University Create_Refresh.sql
+++ 2400/University Create_Refresh.sql
@@ -1,21 +1,24 @@
+/*
 USE master
 GO
 
-/**
 DROP DATABASE IF EXISTS hlawson5_University
 GO
 
 CREATE DATABASE hlawson5_University
 GO
-**/
+*/
 
 USE hlawson5_University
 GO
 
 DROP TABLE IF EXISTS Enrollments
+GO
+
 DROP TABLE IF EXISTS Students
 DROP TABLE IF EXISTS Courses
 DROP TABLE IF EXISTS Instructors
+GO
 
 CREATE TABLE Students(
 	StudentID int IDENTITY NOT NULL PRIMARY KEY,
@@ -46,16 +49,20 @@ CREATE TABLE Instructors(
 	LastName varchar(30) NOT NULL
 )
 
+GO
+
 CREATE TABLE Enrollments(
 	EnrollmentID int IDENTITY NOT NULL PRIMARY KEY,
 	StudentID int NOT NULL,
 	CourseID int NOT NULL,
 	InstructorID int NOT NULL,
 	Grade char(2) NOT NULL CHECK(
-		Grade LIKE '[A,B,C,D]%'
+		Grade LIKE '[A,B,C,D]%' AND (Grade LIKE '%[+,-]' OR RIGHT(Grade, 1) = '')
 	)
 
 	FOREIGN KEY (StudentID) REFERENCES Students(StudentID),
 	FOREIGN KEY (CourseID) REFERENCES Courses(CourseID),
 	FOREIGN KEY (InstructorID) REFERENCES Instructors(InstructorID),
 )
+
+GO
