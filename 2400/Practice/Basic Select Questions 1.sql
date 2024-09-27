e7a64d581e0b80c3ddd18c73cb413ae8a2e4473a
HajayL <84947010+HajayL@users.noreply.github.com>
2024-09-27 13:55:16 -0600

    Added a few more questions to the basic select

--- 2400/Practice/Basic Select Questions 1.sql
+++ 2400/Practice/Basic Select Questions 1.sql
@@ -12,20 +12,29 @@
 -- Q4	- Return all the ages of Employees over 30
 
 
+-- Q5	- Return the 5 youngest employees
+
+
 -- Q5	- Return all customers in cities that start with 'p' or 'is'
 
 
--- Q6	- Return all products that start with 'c' and end with 'e'
+-- Q7	- Return all products that start with 'c' and end with 'e'
 
 
--- Q7	- Return all employees in the format 'TitleofCourtesy LastName' example -> 'Ms. Johnson'
+-- Q8	- Return all employees which's first name contains an 'a' or Last name is 'Pike'
 
 
--- Q8	- Return all orders where the shipped date is at least 10 days after the required day, order by hours taken
+-- Q9	- Return all employees in the format 'TitleofCourtesy LastName' example -> 'Ms. Johnson'
 
 
--- Q9	- Return all customers with a postal code that respects that Canadian postal code system (has a space in the middle) example -> 'TTT 555'
+-- Q10	- Return all orders where the shipped date is at least 10 days after the required day, order by hours taken
 
 
--- Q10	- Return all orders that were made on this day, but with any year
+-- Q11	- Return all customers with a postal code that respects that Canadian postal code system (has a space in the middle) example -> 'TTT 555'
+
+
+-- Q12	- Return all orders that were made on this day, but with any year
+
+
+-- Q13	- Create a variable that stores the city 'Seattle' use that variable to return all employees in that city
 
