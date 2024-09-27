2a41b3bebb1ab80e51d9ac7a94711f6bf076bac9
HajayL <84947010+HajayL@users.noreply.github.com>
2024-09-27 13:39:51 -0600

    Created questiosn 1 for Databases 2400 basic select

---
+++ 2400/Practice/Basic Select Questions 1.sql
@@ -0,0 +1,31 @@
+-- Use Northwind Traders for these questions
+
+-- Q1	- Return all employees who have a Fax
+
+
+-- Q2	- Return all orders that have a shipping region and a ship postal code
+
+
+-- Q3	- Return all customer's who's contact names start with 'M'
+
+
+-- Q4	- Return all the ages of Employees over 30
+
+
+-- Q5	- Return all customers in cities that start with 'p' or 'is'
+
+
+-- Q6	- Return all products that start with 'c' and end with 'e'
+
+
+-- Q7	- Return all employees in the format 'TitleofCourtesy LastName' example -> 'Ms. Johnson'
+
+
+-- Q8	- Return all orders where the shipped date is at least 10 days after the required day, order by hours taken
+
+
+-- Q9	- Return all customers with a postal code that respects that Canadian postal code system (has a space in the middle) example -> 'TTT 555'
+
+
+-- Q10	- Return all orders that were made on this day, but with any year
+
