3f0c14ed0fd10325f0a531de2257642969981f03
HajayL <84947010+HajayL@users.noreply.github.com>
2024-11-22 13:42:44 -0700

    Update ClassTrack_ICA_10_INIT.sql

--- ICA_10/ClassTrack_ICA_10_INIT.sql
+++ ICA_10/ClassTrack_ICA_10_INIT.sql
@@ -19,6 +19,7 @@ DECLARE @ClassId int = @@IDENTITY;
 
 insert into Class_To_Student (class_id, student_id, active) select @ClassId, s.student_id, 1 from students s where s.last_name like '[aeiou]%'
 
+/*
 -- 1.
 
 SELECT
@@ -70,7 +71,8 @@ SELECT
     s.student_id,
     s.last_name,
     s.first_name,
-    s.school_id
+    s.school_id,
+	c.class_desc
 
 FROM
     Students s INNER JOIN
@@ -84,12 +86,115 @@ WHERE
 ORDER BY
     s.last_name,
     s.first_name
+*/
+
+DELETE
+FROM
+	class_to_student
+WHERE
+	class_id IN (SELECT class_id FROM Classes c WHERE class_desc = 'Computing in Airplanes')
+
+SELECT
+	*
+
+FROM
+	Classes c FULL JOIN
+	class_to_student cts ON cts.class_id = c.class_id
+
+WHERE
+	c.class_desc = 'Computing in Airplanes'
+
+DECLARE @_ClassID int;
+DECLARE @_CourseID int;
+DECLARE @_InstructorID int;
+
+SELECT
+	@_ClassId = c.class_id,
+	@_CourseId = c.course_id,
+	@_InstructorID = c.instructor_id
+
+
+FROM
+	Classes c
+
+WHERE
+	c.class_desc = 'Computing in Airplanes'
+
+SELECT
+	@_ClassID 'ClassID',
+	@_CourseID 'CourseID',
+	@_InstructorID 'InstructorID'
+
+DELETE
+FROM
+	Classes
+
+WHERE
+	class_id = @_ClassId
+
+SELECT
+	*
+
+FROM
+	Classes
+
+WHERE
+	class_desc = 'Computing in Airplanes'
+
+DELETE
+FROM
+	Courses
+
+WHERE
+	course_id = @_CourseId
+
+SELECT
+	*
+
+FROM
+	Courses
+
+WHERE
+	course_abbrev = 'CMPE1010'
+
+DELETE
+FROM
+	Instructors
+
+WHERE
+	instructor_id = @InstructorId
+
+SELECT
+	*
+
+FROM
+	Instructors
+
+WHERE
+	last_name = 'Hunterson'
+	
+/*
+SELECT
+    s.student_id,
+    s.last_name,
+    s.first_name,
+    s.school_id,
+	c.class_desc
+
+FROM
+    Students s INNER JOIN
+    class_to_student cts ON cts.student_id = s.student_id INNER JOIN
+    Classes c ON c.class_id = cts.class_id INNER JOIN
+    Instructors i ON i.instructor_id = c.instructor_id
+*/
 
 ROLLBACK
 
 begin transaction
 update students set last_name = last_name + 'x'
 
-SELECT * FROM Students
+--SELECT * FROM Students
+
 rollback
+
 
